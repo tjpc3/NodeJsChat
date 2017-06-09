d5432b141eb7797d14d0eb9f56f0b962788fb941
tjpc3 <tjpc3@users.noreply.github.com>
2017-06-09 11:32:06 -0400

    dgfh

--- app/chatserver.js
+++ app/chatserver.js
@@ -1,17 +1,34 @@
 module.exports = function(io) {
-  io.on('connection', function(socket) {
-    if(socket.request.session.passport == null)
-      socket.disconnect();
-    else {
-      var username = socket.request.session.passport.user.username;
-      io.emit('connected', {name:username});
-      socket.on('message', function(msg) {
-        if (msg.length != 0 && msg.length < 700)
-          io.emit('message', {name:username, message:msg});
-      });
-      socket.on('disconnect', function() {
-        io.emit('disconnected', {name:username});
-      });
-    }
-  });
+    io.on('connection', function(socket) {
+        if (socket.request.session.passport == null)
+            socket.disconnect();
+        else {
+            var username = socket.request.session.passport.user.username;
+            io.emit('connected', {
+                name: username
+            });
+
+            socket.on('message', function(msg) {
+                if (msg.length != 0 && msg.length < 700)
+                    io.emit('message', {
+                        name: username,
+                        message: msg
+                    });
+                console.log("<" + username + "> " + msg);
+            });
+
+            socket.on('disconnect', function() {
+                io.emit('disconnected', {
+                    name: username
+                });
+            });
+
+            socket.on('typing', function(data) {
+                socket.broadcast.emit('typing', {
+                    typing: data.typing,
+                    name: username
+                });
+            });
+        }
+    });
 }
--- public/js/chat.js
+++ public/js/chat.js
@@ -1,31 +1,73 @@
-var socket = io();
-
 function displayMessage(message) {
-  var newMessage = $('<li>').text(message);
-  $('#messages').append(newMessage);
-  $('#messages-container').animate({
-    scrollTop : newMessage.position().top,
-  }, 100);
+    var newMessage = $('<li>').text(message);
+    $('#messages').append(newMessage);
+    $('#messages-container').animate({
+        scrollTop: newMessage.position().top,
+    }, 100);
 }
 
 $(document).ready(function() {
-  $('form').submit(function(){
-    var message = $('#m').val();
-    if (message.length != 0 && message.length < 1000)
-      socket.emit('message', message);
-    $('#m').val('');
-    return false;
-  });
+    var socket = io();
+    var isTyping = false;
+    var typingArr = [];
 
-  socket.on('connected', function(data){
-    displayMessage(data.name + " has connected.");
-  });
+    $('form').submit(function() {
+        var message = $('#m').val();
+        if (message.length != 0 && message.length < 1000)
+            socket.emit('message', message);
+        $('#m').val('');
+        return false;
+    });
 
-  socket.on('disconnected', function(data){
-    displayMessage(data.name + " has disconnected.");
-  });
+    socket.on('connected', function(data) {
+        displayMessage(data.name + " has connected.");
+    });
 
-  socket.on('message', function(data){
-    displayMessage("<" + data.name + "> " + data.message);
-  });
+    socket.on('disconnected', function(data) {
+        displayMessage(data.name + " has disconnected.");
+    });
+
+    socket.on('message', function(data) {
+        displayMessage("<" + data.name + "> " + data.message);
+    });
+
+    socket.on('typing', function(data) {
+        if (data.typing = true)
+            typingArr.append(data.name);
+        else {
+            var index = typingArr.indexOf(5);
+            if (index > -1) {
+                typingArr.splice(index, 1);
+            }
+        }
+
+        var typingMessage = "";
+        for (var i = 0; i < typingArr.length; i++) {
+          if (i = typingArr.length - 1) {
+            typingMessage += typingArr[i];
+          } else {
+            typingMessage += typingArr[i] + ", ";
+          }
+        }
+        typingMessage += " are typing.";
+        $('#typing').val(typingMessage);
+    });
+
+    setInterval(function() {
+        if ($('#m').val() != "") {
+            if (!isTyping) {
+                socket.emit("typing", {
+                    typing: true
+                });
+                isTyping = true;
+            }
+        } else {
+            if (isTyping) {
+                socket.emit("typing", {
+                    typing: false
+                });
+                isTyping = false;
+            }
+        }
+    }, 500);
 });
